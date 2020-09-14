2829314e26bdc6fedf1ab4c8162779a111757f5e
paul1905 <paulmacleod88@gmail.com>
2020-09-14 12:45:07 +1000

    Create main.sh

---
+++ main.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+apt update
+apt -y upgrade
+apt -y install unattended-upgrades
+apt -y autoremove
+
+cp ./50unattended-upgrades /etc/apt/apt.conf.d/
+cp ./20auto-upgrades /etc/apt/apt.conf.d/
+
