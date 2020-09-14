f180294aaa8c7aabc00bb76518cef21a0c0b4294
paul1905 <paulmacleod88@gmail.com>
2020-09-14 14:37:52 +1000

    Update main.sh

--- main.sh
+++ main.sh
@@ -4,7 +4,7 @@ apt update
 apt -y upgrade
 apt -y install unattended-upgrades
 apt -y autoremove
-
 cp ./50unattended-upgrades /etc/apt/apt.conf.d/
 cp ./20auto-upgrades /etc/apt/apt.conf.d/
+unattended-upgrades --dry-run --debug
 
