c69bafba852c9c3f1d143602bd75e4035988c49c
paul1905 <paulmacleod88@gmail.com>
2020-09-14 14:38:46 +1000

    Update main.sh

--- main.sh
+++ main.sh
@@ -2,7 +2,7 @@
 
 apt update
 apt -y upgrade
-apt -y install unattended-upgrades
+apt -y install unattended-upgrades fail2ban
 apt -y autoremove
 cp ./50unattended-upgrades /etc/apt/apt.conf.d/
 cp ./20auto-upgrades /etc/apt/apt.conf.d/
