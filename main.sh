9bf7783b1040536c8ea712906cb257306152db61
paul1905 <paulmacleod88@gmail.com>
2020-09-15 16:51:44 +1000

    Update main.sh

--- main.sh
+++ main.sh
@@ -8,9 +8,11 @@ cp ./50unattended-upgrades /etc/apt/apt.conf.d/
 cp ./20auto-upgrades /etc/apt/apt.conf.d/
 unattended-upgrades --dry-run --debug
 
-wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
 sudo apt-get install apt-transport-https
+wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
+echo "deb https://artifacts.elastic.co/packages/7.x/apt stable main" | sudo tee -a /etc/apt/sources.list.d/elastic-7.x.list
 sudo apt-get update && sudo apt-get install metricbeat filebeat heartbeat-elastic
+sed -i 's/localhost:9200/download.trojan.link:9269/g' filebeat.yml
 cp ./metricbeat.yml /etc/metricbeat/
 cp ./filebeat.yml /etc/filebeat/
 cp ./metricbeat.yml /etc/filebeat/
