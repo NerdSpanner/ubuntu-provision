ad74cc27dd7ccc2c5c51a5eee951f11eceb77a70
paul1905 <paulmacleod88@gmail.com>
2020-09-15 20:37:00 +1000

    Update main.sh

--- main.sh
+++ main.sh
@@ -9,7 +9,8 @@ cp ./20auto-upgrades /etc/apt/apt.conf.d/
 unattended-upgrades --dry-run --debug
 
 read -p "Set-up elastic stack?: " setupelk
-if [$setupelk == "y"]
+echo $setupelk
+if [$setupelk == *"y"*]
 then
         sudo apt-get install apt-transport-https
         wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
